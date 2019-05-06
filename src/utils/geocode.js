d53e60cd28440d7fbe222addce6723e44fc23c19
GinaJ <gina.jiang.93@gmail.com>
2019-05-07 00:24:06 +0200

    update  new URL

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZ2luYWoiLCJhIjoiY2p2Y3BiYWRqMjA5OTRkbzV4MmVmcTY1MyJ9.GGdm85F6mp2kUvH5u8lIsg&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
